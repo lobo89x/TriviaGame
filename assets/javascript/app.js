9079c493f4cb3c1cc752d3857643ac2fed8c02d0
Alexander H Lee III <ahlee1026@gmail.com>
2019-07-09 01:14:15 -0400

    corrected the bug in the jquery. items now show

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,6 +2,8 @@
 var correct = 0;
 var wrong = 0;
 var answerarray = [];
+var guess = "";
+
 
 var questionarray = [
     {
@@ -15,33 +17,57 @@ function quiz (array) {
     for (var i = 0; i < array.length; i++) {
         var questionBlock = $("<div>");
         questionBlock.addClass("row");
-        var hero = $("<button>");
+        // var hero = $("<button>");
         questionBlock.addClass("quiz");
-        // var hero = array[i].HeroName;
+        var hero = array[i].HeroName;
+        // hero.text(array[i].HeroName);
         
-        hero.text(array[i].HeroName);
-
-        $("#quizblock").append(hero);
-        
-        
-        // answerarray = array[i].possibleans;
-        // for (var i = 0; i < answerarray.length; i++) {
-        //     var answerblk = $("<button>");
-        //     answerblk.addClass("btn")
-        //     answerblk.html(answerarray[i]);
-        //     console.log(answerblk);
-        //     console.log(answerarray[i]);
-        //     // $(".quiz").append(answerarray[i]);
-        // }
+        answerarray = array[i].possibleans;
+        for (var i = 0; i < answerarray.length; i++) {
+            var answers = $("<div>");
+            answers.addClass("answerClass btn")
+            answers.html(answerarray[i]);
+            console.log("answers variable  "+answers);
+            console.log("answer array  "+answerarray[i]);
+            answers.attr("answer-val",answerarray[i]);
+            $("#choices").append(answers);
+            // $("#choices").html(answerarray[i]);
+        }
 
         console.log("This is the superhero name "+hero)
-        // questionBlock.addClass("");
-        // $("#quizblock").append(questionBlock);
+        $("#hn").html("<h2>" + hero + "</h2>");
+        
 
-      }
-}
 
-$("#reset").hide();
+
+        // //click function to select answer and outcomes
+        // $(".answerClass").on("click", function () {
+        //     //grab array position from user guess
+        //     guess = parseInt($(this).attr("data-guessvalue"));
+
+        //     //correct guess or wrong guess outcomes
+        //     if (guess === array[i].correctans) {
+        //         stop();
+        //         correct++;
+        //         guess="";
+        //         $("#quizblock").html("<p>Correct!</p>");
+        //     } else {
+        //         stop();
+        //         wrong++;
+        //         guess="";
+        //         $("#quizblock").html("<p>Wrong! The correct answer is: " + array[i].correctans + "</p>");
+        //     }
+        // })
+        }
+return $("#hn").html("<h2>" + hero + "</h2>");
+      
+};
+
+
+
+
+
+
 
 quiz (questionarray);
 
